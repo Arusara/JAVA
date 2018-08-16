afba5f6b7c53cb59355cef1e58594669f83f8136
Arumugam <arumugam@arumugams-air.in.ibm.com>
2018-08-16 15:10:18 +0530

    getting input from user

---
+++ input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class input {
+ public static void main(String arg[]) {
+  Scanner scan=new Scanner(System.in);
+System.out.println("enter the number");  
+int n=scan.nextInt();
+System.out.println("enter the string");
+String str=scan.nextLine();
+ }
+}
