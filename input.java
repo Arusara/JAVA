bb2938836a8dba4784288f808aa750aefddb32cd
Arusara <42410629+Arusara@users.noreply.github.com>
2018-08-16 15:49:25 +0530

    Update input.java

--- input.java
+++ input.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 public class input {
  public static void main(String arg[]) {
+ 
   Scanner scan=new Scanner(System.in);
-System.out.println("enter the number");  
-int n=scan.nextInt();
-System.out.println("enter the string");
-String str=scan.nextLine();
+
+  System.out.println("enter the number");  
+  int n=scan.nextInt();
+  System.out.println("enter the string");
+   String str=scan.nextLine();
+  System.out.println("user entered the values: "+n+" "+str);
+  
+  
+  
  }
 }
