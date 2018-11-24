e953d138a7d6df6a4bba6db117327858a20431e4
Arusara <42410629+Arusara@users.noreply.github.com>
2018-11-24 22:41:36 +0530

    Create DateDemo.java

---
+++ DateDemo.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+
+
+
+public class DateDemo {
+  
+  public static void main(String args[]) {
+    
+    //Date demo
+    
+    Date date = new Date();
+    
+    System.out.println(date.toString());
+  
+  
+  }
+
+
+}
