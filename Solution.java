d2abf1821055a5063c7dadd1b63b10dd14ca8934
Arusara <42410629+Arusara@users.noreply.github.com>
2018-11-24 22:50:13 +0530

    Update and rename Result.java to Solution.java

--- Solution.java
+++ Solution.java
@@ -32,11 +32,31 @@ class Result {
 
         LocalDate dt = LocalDate.of(year, month, day);
         return dt.getDayOfWeek().toString();
-        
-
-
+       
     }
 
 }
 
-public class Solution {
+public class Solution {  
+    
+     public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
